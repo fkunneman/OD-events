b3716f4b71569122744112430f72f2982cc79c19
Florian Kunneman <f.kunneman@let.ru.nl>
2014-02-06 11:27:56 +0100

    fdarf

---
+++ extract_hashtag_frequency.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+
+import sys
+import datetime
+from collections import defaultdict
+import gzip
+
+import time_functions
+
+outfile = sys.argv[1]
+date_column = int(sys.argv[2])
+time_column = int(sys.argv[3])
+infiles = sys.argv[4:]
+
+file_hour = {}
+#order files in time
+for f in infile:
+    if f[-2:] == "gz":
+        infile = gzip.open(f,"rb")
+    else:
+        infile = open(f)
+    tweet = infile.readlines()[0]
+    timeinfo = " ".join([tweet.split(" ")[date_column],tweet.split(" ")[time_column]])
+    tweet_datetime = time_functions.return_datetime(time_info,time=True,setting="vs")
+    file_hour[tweet_datetime] = f
+    infile.close()
+
+# make hashtag frequency list
+for hour in sorted(file_hour.keys()):
+    print file_hour[hour]
+# for each hour
+# for each tweet
+# add hashtag to counter
+
+#cluster similar hashtags
+
+# prune hashtags occuring less then 10 times
+
+# for each set of 1000 hashtags in frequency list
+# count the number of occurences per hour
+# write hour-frequency lists to file 
