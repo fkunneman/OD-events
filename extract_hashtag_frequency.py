34c1f7b0a6a50aa3f76d32485ae7fef87b2600e7
Florian Kunneman <f.kunneman@let.ru.nl>
2014-02-07 12:11:22 +0100

    fdasf

--- extract_hashtag_frequency.py
+++ extract_hashtag_frequency.py
@@ -5,6 +5,7 @@ import datetime
 from collections import defaultdict
 import gzip
 import re
+from pynlpl.statistics import levenshtein
 
 import time_functions
 
@@ -28,12 +29,19 @@ for f in infiles:
             timeinfo = [tweet.split("\t")[date_column],tweet.split("\t")[time_column]]
             tweet_datetime = time_functions.return_datetime(timeinfo[0],time=timeinfo[1],setting="vs")
             for hashtag in re.findall(r' ?(#[^ \n]+) ?',tweet):
+                hashtag = hashtag.lower()
                 hashtag_time[hashtag][tweet_datetime] += 1
                 hashtag_frequency[hashtag] += 1
     infile.close()
 
 #cluster similar hashtags
+hashtags = hashtag_frequency.keys()
+sim_hashtags = []
+for hashtag1 in hashtags:
+    for hashtag2 in hasthags:
+        print hashtag1,hashtag2,levenshtein(hashtag1,hashtag2)
 
+exit()
 # prune hashtags occuring less then 10 times
 for h in sorted(hashtag_frequency, key=hashtag_frequency.get, reverse=True)[:250]:
     print h, hashtag_frequency[h]
