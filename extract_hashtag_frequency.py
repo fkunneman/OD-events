efd59aad2ad8350e6a0cdb3fb32f7ca453f0ca92
Florian Kunneman <f.kunneman@gmail.com>
2014-02-07 15:13:32 +0100

    fdaf

--- extract_hashtag_frequency.py
+++ extract_hashtag_frequency.py
@@ -60,7 +60,7 @@ end_timeinfo = [end_tweet.split("\t")[date_column],end_tweet.split("\t")[time_co
 end_datetime = time_functions.return_datetime(end_timeinfo[0],time=end_timeinfo[1],setting="vs")
 timesegments = []
 current_time = start_datetime
-while current_time <= end_time:
+while current_time <= end_datetime:
     current_time += datetime.timedelta(days=1)
     timesegments.append(current_time) 
 print timesegments
