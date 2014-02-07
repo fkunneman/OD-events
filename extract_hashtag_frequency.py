ada256d3c99af35bf278a7e1d3c13be6af770576
Florian Kunneman <f.kunneman@let.ru.nl>
2014-02-07 17:03:18 +0100

    fdaf

--- extract_hashtag_frequency.py
+++ extract_hashtag_frequency.py
@@ -10,10 +10,11 @@ from pynlpl.statistics import levenshtein
 
 import time_functions
 
-outfile = open(sys.argv[1],"w")
-date_column = int(sys.argv[2])
-time_column = int(sys.argv[3])
-infiles = sys.argv[4:]
+outfile1 = open(sys.argv[1],"w")
+outfile2 = open(sys.argv[2],"w")
+date_column = int(sys.argv[3])
+time_column = int(sys.argv[4])
+infiles = sys.argv[5:]
 
 # make hashtag frequency list
 hashtag_frequency = defaultdict(int)
@@ -53,18 +54,17 @@ for i,h in enumerate(hashtags):
         break
 
 # make hashtag time sequences
-# start_tweet = gzip.open(infiles[0],"rb").readlines()[0]
-# start_timeinfo = [start_tweet.split("\t")[date_column],start_tweet.split("\t")[time_column]]
-# start_datetime = time_functions.return_datetime(start_timeinfo[0],time=start_timeinfo[1],setting="vs")
-# end_tweet = gzip.open(infiles[-1],"rb").readlines()[-1]
-# end_timeinfo = [end_tweet.split("\t")[date_column],end_tweet.split("\t")[time_column]]
-# end_datetime = time_functions.return_datetime(end_timeinfo[0],time=end_timeinfo[1],setting="vs")
-# timesegments = []
-# current_time = start_datetime
-# while current_time <= end_datetime:
-#     current_time += datetime.timedelta(days=1)
-#     timesegments.append(current_time) 
-# print timesegments
+start_tweet = gzip.open(infiles[0],"rb").readlines()[0]
+start_timeinfo = [start_tweet.split("\t")[date_column],start_tweet.split("\t")[time_column]]
+start_datetime = time_functions.return_datetime(start_timeinfo[0],setting="vs")
+end_tweet = gzip.open(infiles[-1],"rb").readlines()[-1]
+end_timeinfo = [end_tweet.split("\t")[date_column],end_tweet.split("\t")[time_column]]
+end_datetime = time_functions.return_datetime(end_timeinfo[0],setting="vs")
+timesegments = [start_datetime]
+current_time = start_datetime
+while current_time <= end_datetime:
+    current_time += datetime.timedelta(days=1)
+    timesegments.append(current_time) 
 
 # hashtag_sequence = defaultdict(list)
 # for h in hashtags[:10]:
@@ -81,15 +81,29 @@ for i,h in enumerate(hashtags):
 
 hashtag_peakscore = []
 for h in hashtags[:freq_bound]:
-    sequence = [hashtag_time[h][d] for d in sorted(hashtag_time[h].keys())]
+    sequence = []
+    hashtag_days = sorted(hashtag_time[h].keys())
+    for day in timesegments:
+        if day in hashtag_days:
+            sequence.append(hashtag_time[h][day])
+        else:
+            sequence.append(0)
+    print hashtag,sequence
     ma = max(sequence)
     median = numpy.median(sequence)
-    score = ma/median
-    hashtag_peakscore.append((h,str(score),"|".join([str(e) for e in sequence]),str(ma),str(median)))
+    mean = numpy.mean(sequence)
+    score1 = ma/median
+    score2 = ma/mean
+    hashtag_peakscore.append((h,str(score1),str(score2),"|".join([str(e) for e in sequence]),str(ma),str(median)))
 
 for y in sorted(hashtag_peakscore,key=lambda x: x[1],reverse=True):
-     outfile.write(" ".join(y) + "\n")
-outfile.close()
+     outfile1.write(" ".join(y) + "\n")
+outfile1.close()
+
+for y in sorted(hashtag_peakscore,key=lambda x: x[2],reverse=True):
+     outfile2.write(" ".join(y) + "\n")
+outfile2.close()
+
 # for each set of 1000 hashtags in frequency list
 # count the number of occurrences per hour
 # write hour-frequency lists to file 
