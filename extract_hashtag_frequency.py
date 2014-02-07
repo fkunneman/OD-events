b648796d17275502c630321eea92574bf89fc6fc
Florian Kunneman <f.kunneman@gmail.com>
2014-02-07 13:40:24 +0100

    fdaf

--- extract_hashtag_frequency.py
+++ extract_hashtag_frequency.py
@@ -54,13 +54,13 @@ for i,h in enumerate(hashtags):
 # make hashtag time sequences
 start_tweet = gzip.open(infiles[0],"rb").readlines()[0]
 start_timeinfo = [start_tweet.split("\t")[date_column],start_tweet.split("\t")[time_column]]
-start_datetime = time_functions.return_datetime(timeinfo[0],time=timeinfo[1],setting="vs")
+start_datetime = time_functions.return_datetime(start_timeinfo[0],time=start_timeinfo[1],setting="vs")
 end_tweet = gzip.open(infiles[-1],"rb").readlines()[-1]
 end_timeinfo = [end_tweet.split("\t")[date_column],end_tweet.split("\t")[time_column]]
-end_datetime = time_functions.return_datetime(timeinfo[0],time=timeinfo[1],setting="vs")
+end_datetime = time_functions.return_datetime(end_timeinfo[0],time=end_timeinfo[1],setting="vs")
 timesegments = []
 current_time = start_datetime
-while current_time <= end_time:
+while current_time <= end_datetime:
     current_time += datetime.timedelta(hours=1)
     timesegments.append(current_time) 
 
