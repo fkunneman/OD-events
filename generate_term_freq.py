42ab7025159cd111bfc12ba358c4706cc3f304d7
Florian Kunneman <f.kunneman@gmail.com>
2014-04-21 17:47:08 +0200

    fix

--- generate_term_freq.py
+++ generate_term_freq.py
@@ -1,20 +1,20 @@
 
-
 import sys
 import codecs
 from collections import defaultdict
 
-outfile = open(sys.arg[1],"w")
+outfile = codecs.open(sys.argv[1],"w","utf-8")
 infiles = sys.argv[2:]
 
 wordfreq = defaultdict(list)
 wordcount = defaultdict(int)
+include = {}
 
 print "counting"
 for i,infile in enumerate(infiles):
     print infile
     read = codecs.open(infile,"r","utf-8")
-    for line in infile.readlines()[1:]:
+    for line in read.readlines()[1:]:
         tokens = line.split("\t")
         term = tokens[0]
         wordcount[term] += 1
@@ -23,19 +23,20 @@ for i,infile in enumerate(infiles):
 print "pruning terms"
 for term in wordcount.keys():
     if wordcount[term] > 7:
-        wordfreq[term] = i * [0]
+        wordfreq[term] = (i+1) * ['0']
+        include[term] = True
+    else:
+        include[term] = False
 
 print "making history"
 for i,infile in enumerate(infiles):
     print infile
     read = codecs.open(infile,"r","utf-8")
-    for line in infile.readlines()[1:]:
+    for line in read.readlines()[1:]:
         tokens = line.split("\t")
         term = tokens[0]
-        try:
-            wordfreq[term][i] = [tokens[1]]
-        except:
-            continue
+        if include[term]:
+            wordfreq[term][i] = tokens[1]
 
 for term in wordfreq.keys():
     outfile.write(term + "\t" + "|".join(wordfreq[term]) + "\n")
