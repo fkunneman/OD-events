4c19fad3f442443d2de20c27e3ff8efc37369d95
Florian Kunneman <f.kunneman@let.ru.nl>
2014-05-15 15:32:12 +0200

    fixie

--- tweets_2_burstylda_input.py
+++ tweets_2_burstylda_input.py
@@ -53,24 +53,30 @@ def write_usertweets(inf,um,userl,ch):
                 if um[tokens[usercol]]:
                     text = tokens[textcol].lower().split(" ")
                     if tokens[langcol] == "dutch" and len(text) >= 3:
-                        filtered = " ".join([x for x in text if x not in sw])
-                        user = tokens[usercol]
-                        date = tokens[datecol]
-                        time = tokens[timecol]
-                        user_tweets[user].append(date + " " + time + ":" + filtered)
+                        filtered = " ".join([x for x in text if x not in sw]) 
+                        if len(filtered) >= 3:
+                            user = tokens[usercol]
+                            date = tokens[datecol]
+                            time = tokens[timecol]
+                            userfilename = outdir + "input/" + re.sub("@","",user) + ".txt"
+                            userfile = codecs.open(userfilename,"a","utf-8")
+                            userfile.write(date + " " + time + ":" + filtered + "\n")
+                            # userfile.write("\n".join(user_tweets[user]) + "\n")
+                            userfile.close()
+                            # user_tweets[user].append(date + " " + time + ":" + filtered)
             except IndexError:
                 print "INDEXERROR!",infile,i,ch
                 continue
             except KeyError:
                 print "KEYERROR!",infile,i,ch
         read.close()
-        if i in range(5,2500,5) or i == len(infiles)-1:
-            for y,user in enumerate(user_tweets.keys()):
-                userfilename = outdir + re.sub("@","",user) + ".txt"
-                userfile = codecs.open(userfilename,"a","utf-8")
-                userfile.write("\n".join(user_tweets[user]) + "\n")
-                userfile.close()
-            user_tweets = defaultdict(list)
+        # if i in range(5,2500,5) or i == len(infiles)-1:
+        #     for y,user in enumerate(user_tweets.keys()):
+        #         userfilename = outdir + "input/" + re.sub("@","",user) + ".txt"
+        #         userfile = codecs.open(userfilename,"a","utf-8")
+        #         userfile.write("\n".join(user_tweets[user]) + "\n")
+        #         userfile.close()
+        #     user_tweets = defaultdict(list)
         print "done",infile,i,"of",len(infiles),"in",ch
 
 print "Generating author-tweetcount dict"
@@ -102,9 +108,9 @@ hundred = True
 #fifty = True
 #twenty = True
 
-filelist500 = codecs.open(outdir + "filelist500.txt","w","utf-8")
-filelist200 = codecs.open(outdir + "filelist200.txt","w","utf-8")
-filelist100 = codecs.open(outdir + "filelist100.txt","w","utf-8")
+fl500 = []
+fl200 = []
+fl100 = []
 user_match = {}
 for i,user_c in enumerate(usertweets_sorted):
     user_match[user_c[0]] = False
@@ -112,18 +118,25 @@ for i,user_c in enumerate(usertweets_sorted):
         userindex = i
 #        print user_c[1],"<100"
     elif user_c[1] < 200: 
-        filelist100.write(re.sub("@","",user_c[0]) + "\n")
+        fl100.append(re.sub("@","",user_c[0]))
 #        print user_c[1],"<200"
     elif user_c[1] < 500:
-        filelist200.write(re.sub("@","",user_c[0]) + "\n")
-        filelist100.write(re.sub("@","",user_c[0]) + "\n")
+        fl200.append(re.sub("@","",user_c[0]))
+        fl100.append(re.sub("@","",user_c[0]))
 #        print user_c[1],"<500"
     else:
-        filelist500.write(re.sub("@","",user_c[0]) + "\n")
-        filelist200.write(re.sub("@","",user_c[0]) + "\n")
-        filelist100.write(re.sub("@","",user_c[0]) + "\n")
+        fl500.append(re.sub("@","",user_c[0]))
+        fl200.append(re.sub("@","",user_c[0]))
+        fl100.append(re.sub("@","",user_c[0]))
 #        print user_c[1],">500"
     user_match
+
+filelist500 = codecs.open(outdir + "filelist500.txt","w","utf-8")
+filelist200 = codecs.open(outdir + "filelist200.txt","w","utf-8")
+filelist100 = codecs.open(outdir + "filelist100.txt","w","utf-8")
+filelist500.write("\n".join(fl500))
+filelist200.write("\n".join(fl200))
+filelist100.write("\n".join(fl100))
 filelist500.close()
 filelist200.close()
 filelist100.close()
