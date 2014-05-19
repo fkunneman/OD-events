ec7196a0dadc59df9425a68e2e6bcc5689eadac8
Florian Kunneman <f.kunneman@gmail.com>
2014-05-19 16:26:03 +0200

    fix

--- filter_ranked_tweets.py
+++ filter_ranked_tweets.py
@@ -9,12 +9,15 @@ new_file = codecs.open(sys.argv[2],"w","utf-8")
 aj = re.compile(r"ajax",re.IGNORECASE)
 fey = re.compile(r"feyenoord",re.IGNORECASE)
 ns = re.compile(r"ns")
+Ns = re.compile(r"Ns")
+nS = re.compile(r"nS")
 user = re.compile(r"USER")
+ing = re.compile(r"ing")
 
 for line in rankfile.readlines():
-    tokens = line.strip.split("\t")
+    tokens = line.strip().split("\t")
     text = tokens[-1]
-    if not aj.search(text) and not fey.search(text) and not ns.search(text) and not user.match(text):
+    if not aj.search(text) and not Ns.search(text) and not nS.search(text) and not fey.search(text) and not ns.search(text) and not user.match(text) and not ing.search(text):
         new_file.write(line)
 rankfile.close()
 new_file.close()
