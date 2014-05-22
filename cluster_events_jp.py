a2ac4715177075965030be2cf6fefea929fd22d8
Florian Kunneman <f.kunneman@let.ru.nl>
2014-05-22 11:18:33 +0200

    new

---
+++ cluster_events_jp.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import argparse
+import codecs
+from collections import defaultdict
+import re
+import numpy
+
+"""
+Script to cluster event terms out of a similarity graph
+"""
+parser = argparse.ArgumentParser(description = "Script to cluster event terms out of a similarity graph")
+parser.add_argument('-i', action = 'store', required = True, help = "the file with the similarity graph")  
+parser.add_argument('-k', action = 'store', type = int, required = True, help = "the k parameter")
+parser.add_argument('-o', action = 'store', required = True, help = "the file to write clusters to")
+
+args = parser.parse_args()
+
+#make term nearest neighbor dict
+infile = codecs.open(args.i,"r","utf-8")
+lines = infile.readlines()
+infile.close()
+bursty_terms = lines[0].strip().split(" ")
+for line in lines[1:2]:
+    similarities = line.strip().split(" ")[1:]
+    nns = sorted(similarities)
+    print nns
+
+
+
+#for each term:
+    #for each nearest neighbor
+        #check if term in NN's of nearest neighbor
+        #if so: add to cluster
+
