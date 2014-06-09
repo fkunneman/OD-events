53b5246115b912026a0d2b768c797a513ec2daf7
Florian Kunneman <f.kunneman@gmail.com>
2014-06-10 00:30:52 +0200

    fix

--- annotations_2_labels.py
+++ annotations_2_labels.py
@@ -10,13 +10,13 @@ cluster_annotations = defaultdict(list)
 for af in annotationfiles:
     print af
     r = open(af)
-    for l in r.readlines()[2:]:
+    for l in r.readlines()[1:]:
 #        print l
         tokens = l.strip().split("\t")
 #        print tokens
-#        print tokens
         try:
             cid = tokens[1]
+     #       print cid
         except:
             continue
         eventlabel = tokens[2][0]
@@ -25,14 +25,14 @@ for af in annotationfiles:
             sociallabel = '1'
         else:
             sociallabel = '0'
-
         if cluster_annotations.has_key(cid):
             cluster_annotations[cid].append(sociallabel)
-            cluster_annotations[1] = eventlabel
+            cluster_annotations[cid][1] = eventlabel
         else:
             cluster_annotations[cid] = [eventlabel,sociallabel]
     r.close()
 
 for cid in cluster_annotations.keys():
+    #print cid,cluster_annotations[cid]
     labelfile.write(cid + "\t" + "\t".join(cluster_annotations[cid]) + "\n")
 labelfile.close()
--- annotations_2_weekly_traintest.py
+++ annotations_2_weekly_traintest.py
@@ -3,6 +3,7 @@ import sys
 import datetime
 import re
 import os
+from collections import defaultdict
 
 annotations = open(sys.argv[1])
 classifierdir = sys.argv[2]
@@ -15,16 +16,17 @@ d = re.compile(r'(\d{2})_(\d{2})_(\d+)')
 for line in annotations.readlines():
     tokens = line.strip().split("\t")
     datesearch = d.search(tokens[0]).groups()
-    date = datetime.date(2013,datesearch[0],datesearch[1])
+    date = datetime.date(2013,int(datesearch[0]),int(datesearch[1]))
     if len(tokens) == 4:
         if tokens[1] == tokens[2] and tokens[1] == '1':
             date_annotations[date].append((datesearch[2],'1'))
         else:
             date_annotations[date].append((datesearch[2],'0'))
     else:
-        date_annotations[date].append(datesearch[2],tokens[1])
+        date_annotations[date].append((datesearch[2],tokens[1]))
 
 i = 0
+da_sorted = sorted(date_annotations.keys())
 date = da_sorted[0]
 while date < datetime.date(2013,8,10):
     for w in range(7):
@@ -32,9 +34,8 @@ while date < datetime.date(2013,8,10):
         window_events[i].extend(date_annotations[d])
     print i,date,d
     i += 1
-    date = date + datetime.timedelta(days = 8)
+    date = date + datetime.timedelta(days = 7)
 
-print window_events
 quit()
 
 outdir = classifier_dir + "week2/"
