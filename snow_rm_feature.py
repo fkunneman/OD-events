e64fa93471c24b11486bdb6d31e679fdbdd1bad6
Florian Kunneman <f.kunneman@gmail.com>
2014-06-11 15:23:56 +0200

    fx

--- snow_rm_feature.py
+++ snow_rm_feature.py
@@ -5,7 +5,7 @@ testfile = open(sys.argv[1])
 lines = testfile.readlines()
 testfile.close()
 
-for i in range(12):
+for i in range(13):
     outfile = open("f" + str(i+2) + "/test.txt","w")
     for l in lines:
         tokens = l.split(":")[0].split(",")
